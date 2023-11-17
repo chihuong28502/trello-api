87a32257f230ad2e9ce17eba6a46589bb6185de8
Chí Hưởng <>
2023-11-17 10:30:06 +0700

    Init Base

---
+++ src/server.js
@@ -0,0 +1,13 @@
+import express from "express";
+
+const app = express();
+const hostname = "localhost";
+const port = 8017;
+
+app.get("/", (req, res) => {
+  res.send("<h1>hello world</h1>");
+});
+
+app.listen(port, hostname, () => {
+  console.log(`hello, running sever at ${hostname}:${port}`);
+});
